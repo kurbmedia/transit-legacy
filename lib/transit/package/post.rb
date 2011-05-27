1e4548c3406fb18accae1219298206ebb1dcf46f
Brent Kirby <brent@kurbmedia.com>
2011-05-27 10:37:20 -0400

    added teaser copy for posts

--- lib/transit/package/post.rb
+++ lib/transit/package/post.rb
@@ -37,6 +37,15 @@ module Transit
         self.post_date.strftime("%B %d, %Y")
       end
       
+      def teaser
+        return self.attributes['teaser'] unless self.attributes.teaser.to_s.blank?
+        textfield = self.contexts.ascending(:position).detect{ |c| c._type.to_s == 'Text' }
+        return "" unless textfield
+        return @teaser_text if @teaser_text 
+        doc = Nokogiri::HTML::DocumentFragment.parse(textfield)
+        @teaser_text = doc.xpath('.//p').first.try(:text).to_s
+      end
+      
     end
     
   end
