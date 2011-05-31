f7532640389f712fddd8cfa8c4b8792f4b0d4d0a
Brent Kirby <brent@kurbmedia.com>
2011-05-30 23:15:59 -0400

    fix bug with outputting content

--- app/helpers/package_helper.rb
+++ app/helpers/package_helper.rb
@@ -5,12 +5,9 @@ module PackageHelper
     if obj.respond_to?(:deliver)
       return obj.deliver
     end
-    def obj.deliver
-      obj.contexts.ascending(:position).map do |field|
-      render_to_string(:partial => "contexts/#{field.class.to_s.underscore}", :format => :html, :locals => { :context => field })
-      end.join("\n").html_safe
-    end
-    obj.deliver
+    obj.contexts.ascending(:position).map do |field|
+      render(:partial => "contexts/#{field.class.to_s.underscore}", :format => :html, :locals => { :context => field })
+    end.join("\n").html_safe
   end
   
 end
