b40898f55a613758b9a05e0d73b0af17d064112b
Brent Kirby <brent@kurbmedia.com>
2011-06-01 20:56:41 -0400

    update initializer to generate controllers before eager laod, but after routing

--- lib/transit/rails/engine.rb
+++ lib/transit/rails/engine.rb
@@ -1,6 +1,8 @@
 require 'rails'
 require 'transit'
 require 'paperclip'
+require 'motr'
+require 'motr/orm/mongoid'
 
 module Transit
   class Engine < Rails::Engine
@@ -10,24 +12,28 @@ module Transit
     # After initialization, dynamically create controllers for models 
     # that have been defined in application routes.
     # 
-    ActiveSupport.on_load(:after_initialize) do
-      # TransitController.send(:include, Transit::Controller::Routing)
-      #             
-      #       Transit::Post.subclasses.each do |sub|
-      #         controller_name = "#{sub.to_s.pluralize}Controller"
-      #         unless Transit.const_defined?(controller_name)
-      #           Transit.const_set(controller_name, Class.new( Transit::PostsController) )
-      #           ActiveSupport::Dependencies::reference(Transit.const_get(controller_name))
-      #         end
-      #       end
-      #       
-      #       Transit::Page.subclasses.each do |sub|
-      #         controller_name = "#{sub.to_s.pluralize}Controller"
-      #         unless Transit.const_defined?(controller_name)
-      #           Transit.const_set(controller_name, Class.new( Transit::PagesController) )
-      #           ActiveSupport::Dependencies::reference(Transit.const_get(controller_name))
-      #         end
-      #       end
+    initializer 'transit.generate_controllers', :after => :build_middleware_stack do
+      Transit::Post.subclasses.each do |sub|
+        controller_name = "#{sub.to_s.pluralize}Controller"
+        unless Transit.const_defined?(controller_name)
+          Transit.const_set(controller_name, Class.new( Transit::PostsController) )
+          ActiveSupport::Dependencies::reference(Transit.const_get(controller_name))
+        end
+      end
+      
+      Transit::Page.subclasses.each do |sub|
+        controller_name = "#{sub.to_s.pluralize}Controller"
+        unless Transit.const_defined?(controller_name)
+          Transit.const_set(controller_name, Class.new( Transit::PagesController) )
+          ActiveSupport::Dependencies::reference(Transit.const_get(controller_name))
+        end
+      end
+    end
+
+    initializer 'transit.controller_hooks' do
+      ActiveSupport.on_load(:after_initialize) do
+        TransitController.send(:include, Transit::Controller::Routing)
+      end
     end
     
     initializer 'paperclip' do
