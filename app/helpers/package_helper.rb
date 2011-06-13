13fe8967318d04bb94ac65a398e7c6fc81ea08ab
Brent Kirby <brent@kurbmedia.com>
2011-06-13 00:55:46 -0400

    fix bugs with js and helpers

--- app/helpers/package_helper.rb
+++ app/helpers/package_helper.rb
@@ -7,8 +7,8 @@ module PackageHelper
     end
     obj.contexts.ascending(:position).map do |field|
       case field
-      when Video then deliver_media_context('video', { video_source: field.body, video_type: field.video_type })
-      when Audio then deliver_media_context('audio', { audio_source: field.body })
+      when Video then deliver_media_context('video', field, { video_source: field.body, video_type: field.video_type })
+      when Audio then deliver_media_context('audio', field, { audio_source: field.body })
       else
         render(:partial => "contexts/#{field.class.to_s.underscore}", :format => :html, :locals => { :context => field }).html_safe
       end
@@ -17,7 +17,7 @@ module PackageHelper
   
   private
   
-  def deliver_media_context(type, attrs = {})
+  def deliver_media_context(type, context, attrs = {})
     content_tag(:div, "", data: { id: "#{type}_player_#{context.id}" }.merge(attrs), class: "#{type}_player")
   end
   
