ca293c0bbc9517eb4a1820e994a077eda876af72
Brent Kirby <brent@kurbmedia.com>
2011-06-19 03:04:14 -0400

    fix bug with newly created fields

--- app/controllers/transit/contexts_controller.rb
+++ app/controllers/transit/contexts_controller.rb
@@ -4,13 +4,13 @@ class Transit::ContextsController < ApplicationController
   helper_method :parent
   
   def new
-    @package = scope_class.find(params["#{scope_name}"])
+    @package = scope_class.find(params["#{scope_name}_id"])
     @context = params[:type].classify.constantize.new
     respond_with(@context)
   end
   
   def destroy
-    @package  = scope_class.find(params["#{scope_name}"])
+    @package  = scope_class.find(params["#{scope_name}_id"])
     @context =  @package.contexts.find(params[:id])
     @context.destroy
     flash[:success] = "The selected field has been removed."
@@ -22,11 +22,11 @@ class Transit::ContextsController < ApplicationController
   end
   
   def scope_class
-    @_scope_class = params.keys.detect{ |k| k.to_s.match(/_id/) }.split("_").first.classify.constantize
+    params.keys.detect{ |k| k.to_s.match(/_id/) }.split("_").first.classify.constantize
   end
   
   def scope_name
-    "#{scope_class.to_s.underscore}_id"
+    scope_class.to_s.underscore
   end
   
 end
