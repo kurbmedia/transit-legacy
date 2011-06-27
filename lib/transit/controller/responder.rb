804cd82f1e159d25e7bd8788732f99301605ddc5
Brent Kirby <brent@kurbmedia.com>
2011-06-27 05:36:02 -0400

    fix bug with responder and keys

--- lib/transit/controller/responder.rb
+++ lib/transit/controller/responder.rb
@@ -4,6 +4,7 @@ require 'responders/flash_responder'
 module Transit
   module Controller
     class Responder < ActionController::Responder
+      mattr_accessor :flash_keys
       
       include Responders::FlashResponder
       Responders::FlashResponder.flash_keys = [:success, :error]
@@ -22,29 +23,10 @@ module Transit
         if set_flash_message?
           set_flash_message! 
           header_hash = {}
-          puts controller.flash.inspect
           controller.flash.each{ |key, value| header_hash.merge!(key => value) }
           controller.response['X-Flash-Messages'] = header_hash.to_json
         end      
         defined?(super) ? super : to_format
-      end
-      
-      protected
-      
-      def set_flash_message!
-        if has_errors?
-          set_flash(:error, @alert)
-          status = Transit::Controller::Responder.flash_keys.last
-        else
-          set_flash(:success, @notice)
-          status = Transit::Controller::Responder.flash_keys.first
-        end
-
-        return if controller.flash[status].present?
-
-        options = mount_i18n_options(status)
-        message = ::I18n.t options[:default].shift, options
-        set_flash(status, message)
       end
       
     end
