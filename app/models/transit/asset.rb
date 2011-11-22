fdd407364fb2490f56283916d20c581dff72a5d9
Brent Kirby <brent@kurbmedia.com>
2011-11-22 00:11:22 -0500

    remove attachment from assets directly

--- app/models/transit/asset.rb
+++ app/models/transit/asset.rb
@@ -2,15 +2,5 @@ require 'mime/types'
 
 module Transit
   class Asset
-    include Paperclip::Glue
-    has_attached_file :file
-    before_post_process :skip_processing_unless_image
-
-    def skip_processing_unless_image
-      if !(file.content_type =~ %r{^(image|(x-)?application)/(x-png|pjpeg|jpeg|jpg|png|gif)$})
-        return false 
-      end
-    end
-
   end
 end
